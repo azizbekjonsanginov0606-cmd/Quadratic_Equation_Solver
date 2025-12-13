cd35ea55d1f99f413d934f6d859420ff58443b43
azizbekjonsanginov0606-cmd <azizbekjonsanginov0606@gmail.com>
2025-12-14 01:04:09 +0500

    Quadratic_Equation_Solver

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Math;
+class Muodilai_kivatrai
+{
+    static void Main()
+    {
+        Console.Write("a=");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("b=");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("c=");
+        double c = Convert.ToDouble(Console.ReadLine());
+        double d = Pow(b,2) - 4 * a * c;
+        double x1, x2;
+        Console.WriteLine($"D = {d}");
+        if (d >= 0)
+        {
+            x1 = (-b + Sqrt(d) )/ (2 * a);
+            x2 = (-b - Sqrt(d)) / (2 * a);
+            Console.Write($"x1 = {x1}");
+            Console.Write($"\tx2 = {x2}");
+        }
+        else
+        {
+            Console.WriteLine("Muodila hal nadorad");
+        }
+    }
+
+}
